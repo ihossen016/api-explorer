a89d95d69f65aaa0a4e9b03320e80d5646ba517c
ihossen016 <ihossen016@gmail.com>
2023-11-03 21:57:18 +0600

    responsive issue fixed

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -5,32 +5,26 @@ function History() {
 
     return (
         <div
-            className={`w-[${
-                mobileWidth ? "70%" : "15%"
-            }] absolute z-50 md:static ${
-                mobileWidth ? "px-5" : "px-3"
-            } md:w-[20%] min-h-screen md:px-6 border-r-2 py-10 overflow-hidden bg-white`}
+            className={`px-5 flex justify-start items-start absolute z-50 md:static min-h-screen md:px-6 border-r-2 py-10 overflow-hidden bg-white`}
         >
-            <div className="flex justify-start items-start gap-4">
-                <img
-                    className="md:hidden"
-                    src="./history.svg"
-                    alt="history-icon"
-                    onClick={() => setMobileWidth(!mobileWidth)}
-                />
-                <div className="w-full">
-                    <h2 className="text-lg font-black text-right md:text-left md:text-2xl">
-                        History
-                    </h2>
-                    <ul className="pt-10">
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                    </ul>
-                </div>
+            <img
+                className=""
+                src="./history.svg"
+                alt="history-icon"
+                onClick={() => setMobileWidth(!mobileWidth)}
+            />
+            <div className={`w-full pr-5 ${!mobileWidth ? "hidden" : ""}`}>
+                <h2 className="text-lg font-black text-right md:text-2xl">
+                    History
+                </h2>
+                <ul className="pt-10">
+                    <li>ashfoahshfohdaofhjposdh</li>
+                    <li>ashfoahshfohdaofhjposdh</li>
+                    <li>ashfoahshfohdaofhjposdh</li>
+                    <li>ashfoahshfohdaofhjposdh</li>
+                    <li>ashfoahshfohdaofhjposdh</li>
+                    <li>ashfoahshfohdaofhjposdh</li>
+                </ul>
             </div>
         </div>
     );
