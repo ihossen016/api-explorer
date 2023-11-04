51d9f8e42f465233157a63aa2950caa2b0f765e6
ihossen016 <ihossen016@gmail.com>
2023-11-04 10:19:05 +0600

    loader added

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import FormHeader from "./FormHeader";
 import FormContent from "./FormContent";
 import FormResponse from "./FormResponse";
+import Loader from "../Loader";
 
 function Form() {
+    const [isLoading, setIsLoading] = useState(false);
     const [requestObj, setRequestObj] = useState({
         method: "GET",
         url: "",
@@ -15,29 +17,39 @@ function Form() {
 
     const handleSubmit = e => {
         e.preventDefault();
+        setIsLoading(true);
+
         console.log(requestObj);
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
     };
 
     return (
-        <div className="w-full py-10 pl-[20%] pr-[5%] md:pl-10">
-            <h1 className="text-3xl font-bold underline">API Explorer</h1>
+        <>
+            {isLoading && <Loader />}
 
-            <form className="py-5" onSubmit={handleSubmit}>
-                <FormHeader
-                    requestObj={requestObj}
-                    setRequestObj={setRequestObj}
-                />
+            <div className="w-full py-10 pl-[20%] pr-[5%] md:pl-10">
+                <h1 className="text-3xl font-bold underline">API Explorer</h1>
 
-                <div className="py-10">
-                    <FormContent
+                <form className="py-5" onSubmit={handleSubmit}>
+                    <FormHeader
                         requestObj={requestObj}
                         setRequestObj={setRequestObj}
                     />
-                </div>
-            </form>
 
-            <FormResponse src={{ status: 200, message: "success" }} />
-        </div>
+                    <div className="py-10">
+                        <FormContent
+                            requestObj={requestObj}
+                            setRequestObj={setRequestObj}
+                        />
+                    </div>
+                </form>
+
+                <FormResponse src={{ status: 200, message: "success" }} />
+            </div>
+        </>
     );
 }
 
