b725099d7cceac70f71cf329571ae49024e18c39
ihossen016 <ihossen016@gmail.com>
2023-11-04 21:45:12 +0600

    new history component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import Form from "./components/form/Form";
 import History from "./components/History";
+import {
+    Drawer,
+    Button,
+    Typography,
+    IconButton,
+    List,
+    ListItem,
+    ListItemSuffix,
+    Card,
+} from "@material-tailwind/react";
+import NewHistory from "./components/NewHistory";
 
 function App() {
+    const [openLeft, setOpenLeft] = useState(false);
+
+    const openDrawer = () => setOpenLeft(true);
+    const closeDrawer = () => setOpenLeft(false);
+
     return (
-        <div className="flex justify-start w-full">
-            <History />
+        <div className="w-full">
+            <Button onClick={openDrawer}>Show History</Button>
+            <NewHistory openLeft={openLeft} closeDrawer={closeDrawer} />
+
+            {/* <History /> */}
 
             <Form />
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AppProvider } from "./AppContext";
+import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AppProvider>
-            <App />
-        </AppProvider>
+        <ThemeProvider>
+            <AppProvider>
+                <App />
+            </AppProvider>
+        </ThemeProvider>
     </React.StrictMode>
 );
