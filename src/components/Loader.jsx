1e74e02a47f26c7c0a984f2937445f209b0c3f57
ihossen016 <ihossen016@gmail.com>
2023-11-04 12:15:12 +0600

    typo

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,10 +4,10 @@ function Loader() {
     return (
         <div className="absolute w-full h-full flex justify-center items-center bg-[#a7a7a7b9] ">
             <div
-                class="inline-block h-20 w-20 animate-spin rounded-full border-8 border-gray-950 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                className="inline-block h-20 w-20 animate-spin rounded-full border-8 border-gray-950 border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status"
             >
-                <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
                     Loading...
                 </span>
             </div>
