ca85a9b4055a42836d0fadda0f39f447f24510ac
ihossen016 <ihossen016@gmail.com>
2023-11-04 09:40:39 +0600

    new folder created for fom components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Form from "./components/Form";
+import Form from "./components/form/Form";
 import History from "./components/History";
 
 function App() {
--- src/components/form/FormContent.jsx
+++ src/components/form/FormContent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Tabs from "./tabs/Tabs";
+import Tabs from "../tabs/Tabs";
 import JsonView from "react18-json-view";
 import "react18-json-view/src/style.css";
 
